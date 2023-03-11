45e88069538def6f62b2db4ff29100954363fcb9
salah <eljettiouisalaheddine@gmail.com>
2023-03-11 13:09:26 +0100

    adding Routes to Login

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ListingController;
+use Illuminate\Auth\Events\Login;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +21,8 @@ Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
 
 Route::resource('listing', ListingController::class);
+
+
+Route::get('login', [AuthController::class, 'create'])->name('Login');
+Route::post('login', [AuthController::class, 'store'])->name('login.store');
+Route::delete('logout', [AuthController::class, 'destroy'])->name('logout');
